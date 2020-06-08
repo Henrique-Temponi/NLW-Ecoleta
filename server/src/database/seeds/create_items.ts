7991e10f11eaaaf335e8d39d61fca490e86eb125
henrique-temponi <tanzenheavely@gmail.com>
2020-06-08 19:44:11 -0300

    Adiciona funcao de seeds

---
+++ server/src/database/seeds/create_items.ts
@@ -0,0 +1,13 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex)
+{
+    await knex('items').insert([
+        { title: 'Lampadas',image: 'lampadas.svg'},
+        { title: 'Pilhas e Baterias',image: 'baterias.svg'},
+        { title: 'Papeis e papelao',image: 'papeis-papelao.svg'},
+        { title: 'Residuos eletronicos',image: 'eletronicos.svg'},
+        { title: 'Residuos organicos',image: 'organicos.svg'},
+        { title: 'Oleo de cozinha',image: 'oleo.svg'},
+    ]);
+}
