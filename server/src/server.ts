408bd148bed301412c3e5584504cc8a9c209427e
henrique-temponi <tanzenheavely@gmail.com>
2020-06-08 08:10:48 -0300

    Adiciona conexão ao database e migration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,52 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('yeet');
+app.use(express.json());
 
-    response.json([
-        'test',
-        'test2',
-        'test',
-        'testt'
-    ]);
-});
+app.use(routes);
 
 app.listen('3333');
+
+
+// const users = [
+//     'test',
+//     'test2',
+//     'test',
+//     'testt'
+// ]
+
+// app.get('/users', (request, response) => {
+//     // console.log('yeet');
+//     const search = String(request.query.search);
+
+//     const fillteredUsers = search ? users.filter(users => users.includes(search)) : users;
+
+//     return response.json(fillteredUsers);
+// });
+
+// app.get('/users/:id', (request, response) => {
+
+//     const id = Number(request.params.id);
+
+//     const user = users[id];
+
+//     return response.json(user);
+
+// });
+
+// app.post('/users', (request, response) => {
+
+//     const data = request.body;
+
+//     console.log(data);
+
+//     const user = {
+//         nome: data.name,
+//         email:data.email
+//     }
+
+//     return response.json(user);
+// });
+
