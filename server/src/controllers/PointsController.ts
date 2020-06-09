36bc91049e62f8391a466961042f959cef079d03
henrique-temponi <tanzenheavely@gmail.com>
2020-06-09 20:49:16 -0300

    Concerta erro knex.transaction

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -80,7 +80,7 @@ class PointsController {
     
         await trx('point_items').insert(pointItems);
 
-        
+        await trx.commit();
     
         return response.json({
             id: point_id,
